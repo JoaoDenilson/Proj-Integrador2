448453138ec8be8abbe08be0e7c32c44edd89594
João Denílson Souza dos Santos <denilsonsantos021@gmail.com>
2019-10-17 14:51:09 -0300

    Update cadasCurso.php

--- view/cadasCurso.php
+++ view/cadasCurso.php
@@ -12,7 +12,7 @@
       <div class="card card-register mx-auto mt-5">
         <div class="card-header">Cadastrar Curso</div>
       <div class="card-body">
-        <form action="<?=url("curso/adicionar");?>" method="post">
+        <form action="<?=url("curso/cadastrar");?>" method="post">
           <div class="form-group">
             <div class="form-row">
               
