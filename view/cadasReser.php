9356ca8d475ba917ff70f6f9f76d040e783043ca
YarlesTorres <yarles57@gmail.com>
2019-10-29 10:11:06 -0300

    Update cadasReser.php

--- view/cadasReser.php
+++ view/cadasReser.php
@@ -14,21 +14,7 @@ use Source\Models\Curso; ?>
 
 <form action="<?=url("disciplina/atualizar");?>" method="POST">
           <div class="form-group">
-            <div class="form-row">
-              <div class="col-md-6">
-                <div class="form-label-group">
-                <input type="text" id="firstName" class="form-control" placeholder="First name" required="on" autofocus="autofocus" name="nomeDisc" value= "<?=$disciplina[0]['nomeDisciplina'];?>">
-                  <label for="firstName">Nome da Disciplina</label>
-                </div>
-              </div>
-              
-              <div class="col-md-6">
-                <div class="form-label-group">
-                <input type="text" id="lastName" class="form-control" placeholder="Sigla da Disciplina" name="siglaDisc" required="on"value= "<?=$disciplina[0]['siglaDisciplina'];?>" >
-                  <label for="lastName">Sigla da Disciplina</label>
-                </div>
-              </div>
-            </div>
+            <div class="form-row">         
 
               <select class="btn btn-info dropdown-toggle" name ="cursoDisc">
                   <option selected="selected" value="<?=$disciplina[0]['idCursoFk'];?>" name="cursoDisc"> Selecione o Curso</option>
@@ -47,6 +33,36 @@ use Source\Models\Curso; ?>
                     <?php
                   endif;?>
               </select>
+
+
+              <select class="btn btn-info dropdown-toggle" name ="cursoDisc">
+                  <option selected="selected" value="<?=$disciplina[0]['idCursoFk'];?>" name="cursoDisc"> Selecione a Dsiciplina</option>
+                  <?php
+                  if($cursos):
+                    foreach($cursos as $curso):
+                  ?>
+                  <?php
+                    echo "<option value='{$curso->getIdCurso()}'> {$curso->getNomeCurso()}</option>";
+                    ?>
+                  <?php
+                    endforeach;
+                  else:
+                      ?>
+                      <h4> Não existem Cursos cadastrados </h4>
+                    <?php
+                  endif;?>
+              </select>
+
+
+              <select class="btn btn-info dropdown-toggle" name ="cursoDisc">
+                  <option selected="selected" value="<?=$disciplina[0]['idCursoFk'];?>" name="cursoDisc"> Selecione o Turno</option>
+                  <option value="1">Manhã</option>
+                  <option value="2">Tarde</option>
+                  <option value="3">Noite</option>
+              </select>
+
+
+
         <input type="hidden" name="id" value="<?=url($disciplina[0]['idDisciplina']);?>">
 
         <button type="submit" class="btn btn-primary">Alterar</button>
