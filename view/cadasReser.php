a1659576cad6e5e072e0d5d88ee7e6ae055da363
FcoGabrielSL <fcogabriel373@gmail.com>
2019-11-04 20:30:00 -0300

    Update cadasReser.php
    yar
    
    Co-Authored-By: YarlesTorres <yarles57@users.noreply.github.com>

--- view/cadasReser.php
+++ view/cadasReser.php
@@ -25,7 +25,7 @@ $v->layout("_themeProf");?>
           <div class="btn-group dropright">
            <select class="btn btn-primary" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" name="idCurso" id="cursoDisc" onchange="buscar_cursos()">
 
-            <option selected="selected"  name="idCurso">
+            <option selected="selected" name="idCurso">
               Lista de Cursos
             </option>
             <div class="dropdown-menu">
@@ -109,72 +109,71 @@ $v->layout("_themeProf");?>
     <div class="w-100" style="margin-bottom: 30px;"></div>
   </div>
 
-  <div class="caixa2">
-    <div class="table-responsive">
-      <label>
-        Selecione o(s) Horario(s) da(s) aulas:: 
-      </label>
-      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-        <tr>
-          <td>Horário</td>                
-          <td>Segunda</td>
-          <td>Terça</td>
-          <td>Quarta</td>
-          <td>Quinta</td>
-          <td>Sexta</td>
-        </tr>
-        <tr>
-          <td>A</td>
-          <td><input type="checkbox" name=horarios[] value="segundaA"></td>
-          <td><input type="checkbox" name=horarios[] value="tercaA"></td>
-          <td><input type="checkbox" name=horarios[] value="quartaA"></td>
-          <td><input type="checkbox" name=horarios[] value="quintaA"></td>
-          <td><input type="checkbox" name=horarios[] value="sextaA"></td>
-        </tr>
-        <tr>
-          <td>B</td>
-          <td><input type="checkbox" name=horarios[] value="segundaB"></td>
-          <td><input type="checkbox" name=horarios[] value="tercaB"></td>
-          <td><input type="checkbox" name=horarios[] value="quartaB"></td>
-          <td><input type="checkbox" name=horarios[] value="quintaB"></td>
-          <td><input type="checkbox" name=horarios[] value="sextaB"></td>
-        </tr>
-        <tr>
-          <td>C</td>
-          <td><input type="checkbox" name=horarios[] value="segundaC"></td>
-          <td><input type="checkbox" name=horarios[] value="tercaC"></td>
-          <td><input type="checkbox" name=horarios[] value="quartaC"></td>
-          <td><input type="checkbox" name=horarios[] value="quintaC"></td>
-          <td><input type="checkbox" name=horarios[] value="sextaC"></td>
-        </tr>
-        <tr>
-          <td>D</td>                
-          <td><input type="checkbox" name=horarios[] value="segundaD"></td>
-          <td><input type="checkbox" name=horarios[] value="tercaD"></td>
-          <td><input type="checkbox" name=horarios[] value="quartaD"></td>
-          <td><input type="checkbox" name=horarios[] value="quintaD"></td>
-          <td><input type="checkbox" name=horarios[] value="sextaD"></td>
-        </tr>
-        </tbody>
-      </table>
-
-    </div>
-  </div>
-
-
-    <div class="caixa3">
-        <div class="form-group">
-            <textarea name="observacao" class="form-control" id="exampleFormControlTextarea1" style="resize: none;" rows="3"></textarea>
-            <label>Caso Necessite de Instalação de Programas, especificar acima:</label>
+      <div class="caixa2">
+        <div class="table-responsive">
+          <label>
+            Selecione o(s) Horario(s) da(s) aulas:: 
+          </label>
+              <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <tr>
+                  <td>Horário</td>                
+                  <td>Segunda</td>
+                  <td>Terça</td>
+                  <td>Quarta</td>
+                  <td>Quinta</td>
+                  <td>Sexta</td>
+                </tr>
+                <tr>
+                  <td>A</td>
+                  <td><input type="checkbox" name=horarios[] value="segundaA"></td>
+                  <td><input type="checkbox" name=horarios[] value="tercaA"></td>
+                  <td><input type="checkbox" name=horarios[] value="quartaA"></td>
+                  <td><input type="checkbox" name=horarios[] value="quintaA"></td>
+                  <td><input type="checkbox" name=horarios[] value="sextaA"></td>
+                </tr>
+                <tr>
+                  <td>B</td>
+                  <td><input type="checkbox" name=horarios[] value="segundaB"></td>
+                  <td><input type="checkbox" name=horarios[] value="tercaB"></td>
+                  <td><input type="checkbox" name=horarios[] value="quartaB"></td>
+                  <td><input type="checkbox" name=horarios[] value="quintaB"></td>
+                  <td><input type="checkbox" name=horarios[] value="sextaB"></td>
+                </tr>
+                <tr>
+                  <td>C</td>
+                  <td><input type="checkbox" name=horarios[] value="segundaC"></td>
+                  <td><input type="checkbox" name=horarios[] value="tercaC"></td>
+                  <td><input type="checkbox" name=horarios[] value="quartaC"></td>
+                  <td><input type="checkbox" name=horarios[] value="quintaC"></td>
+                  <td><input type="checkbox" name=horarios[] value="sextaC"></td>
+                </tr>
+                <tr>
+                  <td>D</td>                
+                  <td><input type="checkbox" name=horarios[] value="segundaD"></td>
+                  <td><input type="checkbox" name=horarios[] value="tercaD"></td>
+                  <td><input type="checkbox" name=horarios[] value="quartaD"></td>
+                  <td><input type="checkbox" name=horarios[] value="quintaD"></td>
+                  <td><input type="checkbox" name=horarios[] value="sextaD"></td>
+                </tr>
+                </tbody>
+              </table>
         </div>
-    </div>
+      </div>
+
+      <div class="caixa3">
+          <div class="form-group">
+            <textarea name="observacao" class="form-control" id="exampleFormControlTextarea1" style="resize: none;" rows="3"></textarea>
+            <label>
+              Caso Necessite de Instalação de Programas, especificar acima:
+            </label>
+          </div>
+      </div>
+
+      <input type="hidden" name="metodo" value="store">
+      <input type="hidden"  name="classe" value="reserva">
+      <input type="submit" value="Pedir" class="btn btn-primary">
+
   </div>
-
-<input type="hidden" name="metodo" value="store">
-<input type="hidden"  name="classe" value="reserva">
-<input type="submit" value="Pedir" class="btn btn-primary">
-
-
 </form>
 
 
