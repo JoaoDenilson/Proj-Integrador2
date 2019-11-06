a1da74be35333d69e85609f6f950e67d4424167f
FcoGabrielSL <fcogabriel373@gmail.com>
2019-11-06 17:04:28 -0300

    Update _themeAdm.php
    Co-Authored-By: YarlesTorres <yarles57@users.noreply.github.com>

--- view/_themeAdm.php
+++ view/_themeAdm.php
@@ -157,6 +157,25 @@ $a= $_SESSION['adm'][1];
             </div>
 
         </li>
+
+                <!------------------------------------ HORARIO   -------------------------------->
+
+        <li class="nav-item dropdown">
+
+            <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-fw fa-folder"></i>
+                <span>Ger. Horário</span>
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+
+                <h6 class="dropdown-header">Gerenciar Horário:</h6>
+                <a class="dropdown-item" href="<?=url("horario");?>">Listar</a>
+                <div class="dropdown-divider"></div>
+            </div>
+
+        </li>
+
     </ul>
 
 
